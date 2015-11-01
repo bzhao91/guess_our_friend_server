5efcc0315a6f4c39e7d82e48c8cb772d9dd4a7ce
bzhao91 <btzhao@wisc.edu>
2015-11-01 04:19:45 +0000

    production envir

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, defaults: {format: :json}
+  resources :users
   get '/hello', to: 'users#hello'
   #get '/users/nickname', to: 'users#get_nickname'
   #post '/usus/change_nickname', to: 'users#change_nickname'
