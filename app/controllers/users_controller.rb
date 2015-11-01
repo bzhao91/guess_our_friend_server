2619b7ce2f2f29285122b8e796a8ea4bfeeb6f79
bzhao91 <btzhao@wisc.edu>
2015-11-01 03:40:14 +0000

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   end
   
   def create
-  
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user.to_json
+    else
+      render json: {errors: @user.errors}
+    end
   end
 
   def update
@@ -15,4 +20,9 @@ class UsersController < ApplicationController
   
   def destroy
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
