8aa8a2e93f400f1aeac489f5dbfc1f3f07e7ab4d
bzhao91 <btzhao@wisc.edu>
2015-11-01 04:05:35 +0000

    format json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, defaults: {format: :json}
   #get '/users/nickname', to: 'users#get_nickname'
   #post '/usus/change_nickname', to: 'users#change_nickname'
 end
