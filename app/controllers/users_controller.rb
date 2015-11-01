5da93962b91e4a55b082f6b0dd1ebc839735d0e8
bzhao91 <btzhao@wisc.edu>
2015-11-01 04:39:01 +0000

    create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    render text: "create" and return
     @user = User.new(user_params)
     if @user.save
       render json: @user.to_json
