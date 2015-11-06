65ecee2e77a973b98818b3f73b0478f7eed7c7e7
bzhao91 <btzhao@wisc.edu>
2015-11-06 04:00:01 +0000

    challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,5 @@
 class Challenge < ActiveRecord::Base
     belongs_to :challenger, class_name: "User"
     belongs_to :challengee, class_name: "User"
+    validates_uniqueness_of :challenger_id, :scope => [:challengee_id]
 end
