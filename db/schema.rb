09bf90ea346c60b517658fe9f1782da44699c178
bzhao91 <btzhao@wisc.edu>
2015-11-10 01:24:05 +0000

    populate mutual friend list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110011922) do
+ActiveRecord::Schema.define(version: 20151110012333) do
 
   create_table "challenges", force: :cascade do |t|
     t.integer  "challenger_id"
@@ -71,12 +71,12 @@ ActiveRecord::Schema.define(version: 20151110011922) do
     t.integer  "answer",     default: -1
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.integer  "users_id"
     t.integer  "game_id"
+    t.integer  "user_id"
   end
 
   add_index "questions", ["game_id"], name: "index_questions_on_game_id"
-  add_index "questions", ["users_id"], name: "index_questions_on_users_id"
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
