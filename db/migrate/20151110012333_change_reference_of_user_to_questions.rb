6f6f562595ccaa9411836221971b13c920c40692
bzhao91 <btzhao@wisc.edu>
2015-11-10 01:24:10 +0000

    populate mutual friend list

---
+++ db/migrate/20151110012333_change_reference_of_user_to_questions.rb
@@ -0,0 +1,6 @@
+class ChangeReferenceOfUserToQuestions < ActiveRecord::Migration
+  def change
+    remove_reference :questions, :users
+    add_reference :questions, :user, :index => true
+  end
+end
