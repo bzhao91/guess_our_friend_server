a90f6190d41a990ada91bd13e3236fefa8008aeb
bzhao91 <btzhao@wisc.edu>
2015-12-02 02:51:39 +0000

    ongoing save

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ActiveRecord::Base
     belongs_to :player2, class_name: "User"
     validates :player1id, presence: true
     validates :player2id, presence: true
-    #validates_uniqueness_of :player1id, :scope => [:player2id]
-    #validates_uniqueness_of :player2id, :scope => [:player1id]
+    validates_uniqueness_of :player1id, :scope => [:player2id]
+    validates_uniqueness_of :player2id, :scope => [:player1id]
 end
