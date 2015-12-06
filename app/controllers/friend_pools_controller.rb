7f3131ae9e2a942adefa0eaa45fe2d4fc0833598
bzhao91 <btzhao@wisc.edu>
2015-12-06 20:59:08 +0000

    mystery friend fb_id

--- app/controllers/friend_pools_controller.rb
+++ app/controllers/friend_pools_controller.rb
@@ -87,16 +87,22 @@ class FriendPoolsController < AuthController
     if found == false
       render json: {errors: "Mystery friend is not in friend pool."}, :status => 820 and return
     end
-    
+    game_hash = JSON.parse(g.to_json)
+    game_hash['player1id'] = User.find_by_id(g.player1id).fb_id
+    game_hash['player2id'] = User.find_by_id(g.player2id).fb_id
     if @cur_as_p1 == true
       if @game.mystery_friend1 == -1
         @game.update_attribute(:mystery_friend1, mystery_friend_id)
+        game_hash['mystery_friend1'] = params[:mystery_friend].to_s
+        game_hash['mystery_friend2'] = -1
       else
         render json: {errors: "You cannot reset the mystery friend."}, :status => 820 and return
       end
     else
       if @game.mystery_friend2 == -1
         @game.update_attribute(:mystery_friend2, mystery_friend_id)
+        game_hash['mystery_friend1'] = -1
+        game_hash['mystery_friend2'] = params[:mystery_friend].to_s
       else
         render json: {errors: "You cannot reset the mystery friend."}, :status => 820 and return
       end
@@ -104,9 +110,9 @@ class FriendPoolsController < AuthController
     if @game.mystery_friend1 != -1 && @game.mystery_friend2 != -1
       @game.update_attribute(:state, 1)
     end
-
+    
     send_gcm_message(@opponent.gcm_id, "Guess the Mystery Friend!", {pool: result, game_id: @game.id}.to_json)
-    render json: {game: @game}
+    render json: {game: game_hash}
   end
   
 private
