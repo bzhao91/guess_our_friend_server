52cd6d4ce57b747df6314cf8d92cfe3fb1067eb8
bzhao91 <btzhao@wisc.edu>
2015-12-06 02:18:13 +0000

    index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < AuthController
   skip_before_action :verify_authenticity_token
   before_action :login?, :except => [:create]
   
+  def index
+    render json: User.all
+  end
   def create
     @user = User.new(user_params)
     if @user.save
