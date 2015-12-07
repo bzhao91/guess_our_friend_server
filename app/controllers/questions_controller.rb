5a39eba97e7f3a3a1f120d3795ff5fd327380d51
bzhao91 <btzhao@wisc.edu>
2015-12-07 06:14:32 +0000

    both players need to set mystery friend

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -135,10 +135,12 @@ private
     end
     
     def set_game
-       
         @game = Game.find_by_id(params[:game_id])
         unless @game
             render json: {errors: "Game does not exist"}, :status => 810 and return
+        end
+        if @game.state != 1
+            render json: {errors: "Both players need to set mystery friends before game starts"}, :status => 811 and return
         end
         if @current_user.id != @game.player1id && @current_user.id != @game.player2id
             render json: {errors: "Invalid game"}, :status => 811 and return
